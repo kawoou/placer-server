2119cbe5dbc7c9edb85ae0c4cc756d8e80a95754
minseungkang <alstmd3@naver.com>
2019-09-28 12:12:14 +0900

    initialize project files

---
+++ src/main/java/capstone/placer/PlacerApplication.java
@@ -0,0 +1,13 @@
+package capstone.placer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacerApplication.class, args);
+    }
+
+}
