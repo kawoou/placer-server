4fa58cd16ba10d3a92df2a797f070193d38a4f87
minseungkang <alstmd3@naver.com>
2019-10-27 15:18:19 +0900

    spring boot에서 war 파일 추출을 위해 추가로 필요한 코드 작성

--- src/main/java/capstone/placer/PlacerApplication.java
+++ src/main/java/capstone/placer/PlacerApplication.java
@@ -2,12 +2,18 @@ package capstone.placer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlacerApplication {
+public class PlacerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PlacerApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PlacerApplication.class);
+    }
 }
+
