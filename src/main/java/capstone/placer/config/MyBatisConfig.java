551c897db937db67a09e4e2f17c8ee56b5998e16
minseungkang <alstmd3@naver.com>
2019-10-27 12:34:10 +0900

    configuring

--- src/main/java/capstone/placer/config/MyBatisConfig.java
+++ src/main/java/capstone/placer/config/MyBatisConfig.java
@@ -1,9 +1,25 @@
 package capstone.placer.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 @MapperScan(basePackages="capstone.placer")
-public class MyBatisConfig {
+public class MyBatisConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
