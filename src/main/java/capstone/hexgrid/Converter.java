02f916635ed22df21ec50f9fef87d01cec0d8926
RKTP <archetype93@gmail.com>
2019-11-11 21:49:13 +0900

    [DEV] gps converter logic refine

--- src/main/java/capstone/hexgrid/Converter.java
+++ src/main/java/capstone/hexgrid/Converter.java
@@ -1,5 +1,8 @@
 package capstone.hexgrid;
 
+import java.util.ArrayList;
+
 interface Converter {
-    Hex pointToHex(Point p_);
+    Hex pointToHex(Point p_, int level);
+    ArrayList<Hex> pointToHex(Point p_);
 }
