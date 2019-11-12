e49e2cf41883b7c477e37a882dbbb38544a251d2
minseungkang <alstmd3@naver.com>
2019-11-12 20:46:18 +0900

    S3 Util 수정

--- src/main/java/capstone/placer/util/S3Util.java
+++ src/main/java/capstone/placer/util/S3Util.java
@@ -1,5 +1,6 @@
 package capstone.placer.util;
 
+import capstone.placer.config.PropertyLoader;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
@@ -9,28 +10,31 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 public class S3Util {
 
-    @Value("${cloud.aws.credentials.accessKey}")
+    private static final String PROPERTY_FILE_NAME = "s3.properties";
     private String ACCESS_KEY;
-    @Value("${cloud.aws.credentials.secretKey}")
     private String PRIVATE_KEY;
-
     private AmazonS3 s3Connection;
 
-    public S3Util() {
+    public S3Util() throws IOException {
+        Properties s3Property = PropertyLoader.loadProperties(PROPERTY_FILE_NAME);
+        ACCESS_KEY = s3Property.getProperty("cloud.aws.credentials.accessKey");
+        PRIVATE_KEY = s3Property.getProperty("cloud.aws.credentials.secretKey");
+
         AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, PRIVATE_KEY);
         ClientConfiguration conf = new ClientConfiguration();
         conf.setProtocol(Protocol.HTTP);
         s3Connection = new AmazonS3Client(credentials, conf);
-        s3Connection.setEndpoint("");
+        s3Connection.setEndpoint("s3.ap-northeast-2.amazonaws.com");
     }
 
     public List<Bucket> getBuckets() {
