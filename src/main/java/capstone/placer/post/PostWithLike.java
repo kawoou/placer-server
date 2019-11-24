60d045e6453aa6daffd57500c84e8ea4d7c55319
minseungkang <alstmd3@naver.com>
2019-11-24 23:29:13 +0900

    [DEV] get data to getbyPopularity fix

--- src/main/java/capstone/placer/post/PostWithLike.java
+++ src/main/java/capstone/placer/post/PostWithLike.java
@@ -9,7 +9,7 @@ public class PostWithLike extends Post {
     }
 
     public PostWithLike(Post post, boolean myLike) {
-        super(post.getWriterNickName(), post.getAddress(), post.getComment());
+        super(post.getId(), post.getWriterNickName(), post.getAddress(), post.getLike(), post.getCreatedAt(), post.getComment());
         this.myLike = myLike;
     }
 
